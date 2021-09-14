9b2ce8caae13f0971442a55c6e8b7cec87817a15
dung1308 <90315963+dung1308@users.noreply.github.com>
2021-09-14 09:07:56 +0700

    Add files via upload

---
+++ Exercise0301/src/exercise/Calculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Calculator {
+                public int sum(int number1, int number2) {
+			return (number1 + number2);
+		}
+		public int sub(int number1, int number2) {
+			return (number1 - number2);
+		}
+		public int mul(int number1, int number2) {
+			return (number1 * number2);
+		}
+		public int div(int number1, int number2) {
+			return (number1 / number2);
+		}
+}
